992abf42f8cc35d1d7dc620c9f87448c189b17e0
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-07-27 23:08:54 +0530

    Update RemoveDuplicatesFromSortedArray.java

--- leetcode/Arrays/RemoveDuplicatesFromSortedArray.java
+++ leetcode/Arrays/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,5 @@
 //leetcode 26
-class Solution {
+/*class Solution {
     public int removeDuplicates(int[] nums) {
        int j=1;
        for(int i=1 ; i<nums.length ; i++){
@@ -12,3 +12,27 @@ class Solution {
 
     }
 }
+*/
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       int arr[] = {1,1,2,2,2,3,3};
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
