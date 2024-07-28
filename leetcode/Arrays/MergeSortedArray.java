1e166ddb2bfb4e7a77a232f84ae9aed7f7b9e869
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-07-28 21:46:06 +0530

    Create MergeSortedArray.java

---
+++ leetcode/Arrays/MergeSortedArray.java
@@ -0,0 +1,12 @@
+//lc88
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for(int j=0 ,i =m; j<nums2.length;j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+        
+
+    }
+}
