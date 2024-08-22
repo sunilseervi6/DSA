ec16808e960de693159e9bb3482b7347ee857ab2
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-08-22 22:54:59 +0530

    Create NumberComplement.java

---
+++ leetcode/BitManipulation/NumberComplement.java
@@ -0,0 +1,8 @@
+//leetcode 476
+class Solution {
+    public int findComplement(int num) {
+       int len = (int)(Math.log(num) / Math.log(2)) + 1;
+       int mask = (1<<len)-1;
+       return num^mask; 
+    }
+}
