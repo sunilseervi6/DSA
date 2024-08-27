de1a9a2a88fe00c609f58b5eae40335c617f08b7
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-08-28 00:09:59 +0530

    Create ValidParanthesis.java

---
+++ leetcode/Strings/ValidParanthesis.java
@@ -0,0 +1,20 @@
+//leetcode 20
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch =='(' || ch=='[' || ch=='{'){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if(( ch==')' && top =='(' ) || ( ch==']' && top =='[' ) ||( ch=='}' && top =='{' )   ) continue;
+                else return false;
+                
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
