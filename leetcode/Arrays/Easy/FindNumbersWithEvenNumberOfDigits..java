dfc66b9be8043fa77d1098f60fbdfad352ab1518
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-08-30 22:17:19 +0530

    Update FindNumbersWithEvenNumberOfDigits..java

--- leetcode/Arrays/Easy/FindNumbersWithEvenNumberOfDigits..java
+++ leetcode/Arrays/Easy/FindNumbersWithEvenNumberOfDigits..java
@@ -1,5 +1,6 @@
 //leetcode 1295
 //Find Numbers with Even Number of Digits
+https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
 class Solution {
     public int findNumbers(int[] nums) {
         int count =0;
