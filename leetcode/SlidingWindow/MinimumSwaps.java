83ff32bf0a6d2e883b1fe2ec767734c0ca43f7a0
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2024-09-04 23:43:39 +0530

    Create MinimumSwaps.java

---
+++ leetcode/SlidingWindow/MinimumSwaps.java
@@ -0,0 +1,37 @@
+//leetcode 2134
+class Solution {
+    public int minSwaps(int[] nums) {
+        //window size of count 1
+        int windowSize = 0 ;
+        for(int num : nums){
+            windowSize += num;
+        }
+        //find zeros in 1st window
+        int currZeros = 0;
+        for(int i=0 ; i<windowSize ; i++){
+            if(nums[i] == 0){
+                currZeros++;
+            }
+        }
+        //for the remaining windows
+        int minZeros = currZeros;
+        int start = 0 ;
+        int end = windowSize-1;
+        int n = nums.length;
+        while(start < n){
+            //if removed element was zero decrement 0 counter
+            if(nums[start] == 0){
+                currZeros--;
+            }
+            start++;
+            //if included element is 0 increment 0 counter
+            end++;
+            if(nums[end % n] ==0 ){
+                currZeros++;
+            }
+            minZeros = Math.min(minZeros , currZeros);
+        }
+        return minZeros;
+
+    }
+}
