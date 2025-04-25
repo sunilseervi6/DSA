40722220763bc892a7952341aa062953fe5a4504
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2025-04-26 00:26:08 +0530

    Create setMatrixZeo.java

---
+++ leetcode/2dArrays/setMatrixZeo.java
@@ -0,0 +1,25 @@
+//leetcode 73
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix[0].length;
+        int n = matrix.length;
+        boolean row[] = new boolean[n];
+        boolean col[] = new boolean[m];
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m;j++){
+                if (matrix[i][j] == 0){
+                    row[i]= true;
+                    col[j]=true;
+                }
+            }
+        }
+        for(int i =0 ; i<n ; i++){
+            for(int j=0 ; j< m ;j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+ 
+}
